d0b104eaae7a53d6b7f3a45c41061230cde06e6e
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-14 20:38:16 +0900

    PlayerNameString을 Replication에 등록

--- Source/BaseballProject/Player/BBPlayerState.cpp
+++ Source/BaseballProject/Player/BBPlayerState.cpp
@@ -2,3 +2,19 @@
 
 #include "Player/BBPlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+ABBPlayerState::ABBPlayerState()
+{
+	// 1. Replication을 위해 이 Actor의 bReplicates를 true로 설정
+	bReplicates = true;
+}
+
+void ABBPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 3. DOREPLIFETIME 매크로를 사용해 복제할 속성을 명시
+	// "Net/UnrealNetwork.h" 헤더파일 필요
+	DOREPLIFETIME(ThisClass, PlayerNameString);
+}
--- Source/BaseballProject/Player/BBPlayerState.h
+++ Source/BaseballProject/Player/BBPlayerState.h
@@ -10,7 +10,14 @@ UCLASS()
 class BASEBALLPROJECT_API ABBPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+public:
+	ABBPlayerState();
+
+	// 3. GetLifetimeReplicatedProps() 함수에 네트워크로 복제할 속성을 추가
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
 public:
+	// 2. Replication을 위해 UPROPERTY에 Replicated 키워드 추가
+	UPROPERTY(Replicated)
 	FString PlayerNameString;
 };
