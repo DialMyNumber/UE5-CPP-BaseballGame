50ec35e7f20882b2d9d6acee09b1e1a681872ccd
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-14 20:54:23 +0900

    시도 횟수 / 최대 시도 횟수 추가

--- Source/BaseballProject/Player/BBPlayerState.cpp
+++ Source/BaseballProject/Player/BBPlayerState.cpp
@@ -5,6 +5,9 @@
 #include "Net/UnrealNetwork.h"
 
 ABBPlayerState::ABBPlayerState()
+	: PlayerNameString(TEXT("None"))
+	, CurrentGuessCount(0)
+	, MaxGuessCount(7)
 {
 	// 1. Replication을 위해 이 Actor의 bReplicates를 true로 설정
 	bReplicates = true;
@@ -17,4 +20,15 @@ void ABBPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>&
 	// 3. DOREPLIFETIME 매크로를 사용해 복제할 속성을 명시
 	// "Net/UnrealNetwork.h" 헤더파일 필요
 	DOREPLIFETIME(ThisClass, PlayerNameString);
+	DOREPLIFETIME(ThisClass, CurrentGuessCount);
+	DOREPLIFETIME(ThisClass, MaxGuessCount); 
+
+	// 변수 중에서 변경될 여지가 있으면 Replication에 등록,
+	// 변경될 여지 없이 고정 값이라면 Replication에 등록할 필요가 없음
+}
+
+FString ABBPlayerState::GetPlayerInfoString()
+{
+	FString PlayerInfoString = PlayerNameString + TEXT("(") + FString::FromInt(CurrentGuessCount) + TEXT(" / ") + FString::FromInt(MaxGuessCount) + TEXT(")");
+	return PlayerInfoString;
 }
--- Source/BaseballProject/Player/BBPlayerState.h
+++ Source/BaseballProject/Player/BBPlayerState.h
@@ -11,13 +11,23 @@ class BASEBALLPROJECT_API ABBPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 public:
+	// 1. Replication을 위해
 	ABBPlayerState();
 
 	// 3. GetLifetimeReplicatedProps() 함수에 네트워크로 복제할 속성을 추가
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
-	
+
+	// 플레이어의 정보를 가져오는 함수
+	FString GetPlayerInfoString();
+
 public:
-	// 2. Replication을 위해 UPROPERTY에 Replicated 키워드 추가
+	// 2. UPROPERTY에 Replicated 키워드 추가
 	UPROPERTY(Replicated)
 	FString PlayerNameString;
+
+	UPROPERTY(Replicated)
+	int32 CurrentGuessCount;
+
+	UPROPERTY(Replicated)
+	int32 MaxGuessCount;
 };
